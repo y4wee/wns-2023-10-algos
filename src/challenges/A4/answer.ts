9cb3043b84937ac5f203854dbef0c98f4bb0cb52
y4wee <quentin.jambert@gmail.com>
2023-11-27 11:15:08 +0100

    answer B4 - A4

--- src/challenges/A4/answer.ts
+++ src/challenges/A4/answer.ts
@@ -49,19 +49,81 @@
  */
 
 // ↓ uncomment bellow lines and add your response!
-/* 
-export default function ({ ads }: { ads: Ad[] }): MonthAds[] {
-  return [];
-}
- */
+// solution 1
+// export default function ({ ads }: { ads: Ad[] }): MonthAds[] {
+// 	const dateList: Date[] = [];
+// 	ads.forEach((ad) => {
+// 		const createdDate = new Date(ad.createdAt);
+// 		createdDate.setUTCDate(1);
+// 		createdDate.setUTCHours(0, 0, 0, 0);
+// 		if (!dateList.some((el) => el.getTime() === createdDate.getTime())) {
+// 			dateList.push(createdDate);
+// 		}
+// 	});
+// 	dateList.sort((a, b) => {
+// 		return a.getTime() - b.getTime();
+// 	});
+// 	const response = dateList.map((date) => {
+// 		return {
+// 			month: `${date.getUTCFullYear()}-${String(
+// 				date.getUTCMonth() + 1
+// 			).padStart(2, "0")}-01T00:00:00.000Z`,
+// 			ads: ads
+// 				.filter((element) => {
+// 					let elementDate = new Date(element.createdAt);
+// 					return (
+// 						elementDate.getUTCMonth() + 1 ===
+// 							date.getUTCMonth() + 1 &&
+// 						elementDate.getUTCFullYear() === date.getUTCFullYear()
+// 					);
+// 				})
+
+// 				.sort((a, b) => {
+// 					return (
+// 						new Date(b.createdAt).getTime() -
+// 						new Date(a.createdAt).getTime()
+// 					);
+// 				}),
+// 		};
+// 	});
+// 	return response;
+// }
+
+// solution 2
+// export default function ({ ads }: { ads: Ad[] }): MonthAds[] {
+// 	let response = {};
+// 	ads.forEach((ad) => {
+// 		let date = new Date(ad.createdAt);
+// 		let key = `${date.getUTCFullYear()}-${String(
+// 			date.getUTCMonth() + 1
+// 		).padStart(2, "0")}-01T00:00:00.000Z`;
+// 		if (!response[key]) {
+// 			response[key] = {
+// 				month: key,
+// 				ads: [],
+// 			};
+// 		}
+// 		response[key].ads.push({ ...ad });
+// 		response[key].ads.sort((a, b) => {
+// 			return (
+// 				new Date(b.createdAt).getTime() -
+// 				new Date(a.createdAt).getTime()
+// 			);
+// 		});
+// 	});
+// 	const resValue = Object.values(response) as MonthAds[];
+// 	return resValue.sort((a, b) => {
+// 		return new Date(a.month).getTime() - new Date(b.month).getTime();
+// 	});
+// }
 
 // used interfaces, do not touch
 export interface Ad {
-  title: string;
-  createdAt: string;
+	title: string;
+	createdAt: string;
 }
 
 export interface MonthAds {
-  month: string;
-  ads: Ad[];
+	month: string;
+	ads: Ad[];
 }
--- src/challenges/B4/answer.ts
+++ src/challenges/B4/answer.ts
@@ -28,20 +28,88 @@
  */
 
 // ↓ uncomment bellow lines and add your response!
-/* 
-export default function ({ messages }: { messages: Message[] }): DayMessages[] {
-  return [];
-}
- */
+// solution 1
+// export default function ({ messages }: { messages: Message[] }): DayMessages[] {
+// 	const dateList: Date[] = [];
+// 	messages.forEach((message) => {
+// 		if (message.content) {
+// 			let sentDate = new Date(message.sentAt);
+// 			sentDate.setUTCHours(0, 0, 0, 0);
+// 			if (!dateList.some((el) => el.getTime() === sentDate.getTime())) {
+// 				dateList.push(sentDate);
+// 			}
+// 		}
+// 	});
+// 	dateList.sort((a, b) => {
+// 		return a.getTime() - b.getTime();
+// 	});
+// 	const response = dateList.map((date) => {
+// 		return {
+// 			day: `${date.getUTCFullYear()}-${String(
+// 				date.getUTCMonth() + 1
+// 			).padStart(2, "0")}-${String(date.getUTCDate()).padStart(
+// 				2,
+// 				"0"
+// 			)}T${String(date.getUTCHours()).padStart(2, "0")}:${String(
+// 				date.getUTCMinutes()
+// 			).padStart(2, "0")}:${String(date.getUTCSeconds()).padStart(
+// 				2,
+// 				"0"
+// 			)}.${String(date.getUTCMilliseconds()).padStart(3, "0")}Z`,
+// 			messages: messages
+// 				.filter((element) => {
+// 					let elementDate = new Date(element.sentAt);
+// 					elementDate.setUTCHours(0, 0, 0, 0);
+// 					return elementDate.getTime() === date.getTime();
+// 				})
+
+// 				.sort((a, b) => {
+// 					return (
+// 						new Date(a.sentAt).getTime() -
+// 						new Date(b.sentAt).getTime()
+// 					);
+// 				}),
+// 		};
+// 	});
+// 	return response;
+// }
+
+// solution 2
+// export default function ({ messages }: { messages: Message[] }): DayMessages[] {
+// 	let response = {};
+// 	messages.forEach((message) => {
+// 		let date = new Date(message.sentAt);
+// 		let key = `${date.getUTCFullYear()}-${String(
+// 			date.getUTCMonth() + 1
+// 		).padStart(2, "0")}-${String(date.getUTCDate()).padStart(
+// 			2,
+// 			"0"
+// 		)}T00:00:00.000Z`;
+// 		if (!response[key]) {
+// 			response[key] = {
+// 				day: key,
+// 				messages: <Message[]>[],
+// 			};
+// 		}
+// 		response[key].messages.push({ ...message });
+// 		response[key].messages.sort((a, b) => {
+// 			return new Date(a.sentAt).getTime() - new Date(b.sentAt).getTime();
+// 		});
+// 	});
+// 	const resValue = Object.values(response) as DayMessages[];
+// 	return resValue.sort((a, b) => {
+// 		return new Date(a.day).getTime() - new Date(b.day).getTime();
+// 	});
+// }
 // used interfaces, do not touch
 export interface Message {
-  content: string;
-  sentBy: string;
-  sentAt: string;
-  message: string;
+	content: string;
+	sentBy: string;
+	sentAt: string;
+	message: string;
 }
 
 export interface DayMessages {
-  day: string;
-  messages: Message[];
+	day: string;
+	messages: Message[];
 }
